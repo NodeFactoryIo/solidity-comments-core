430b953685a4f372024019e6bc5065e5d90ea6f6
Marin Petrunić <marin.petrunic@gmail.com>
2018-07-09 15:08:12 +0200

    fixes yarn build

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var name = 'ES6 Starter Kit',
+    version = '1.0';
+
+var myObject = {
+  name: name,
+  version: version
+};
+
+exports.default = myObject;
+//# sourceMappingURL=index.js.map
